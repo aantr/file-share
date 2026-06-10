b7468a8247ca77153d82ec9cc8aedd8e7dfff77d
aantr <xgamestudio8@gmail.com>
2026-06-10 13:23:59 +0500

    update ui

--- obj/Debug/net8.0/FileShareExpert.AssemblyInfo.cs
+++ obj/Debug/net8.0/FileShareExpert.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FileShareExpert")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74dab6a3c8f14380e2e1b67fe1f9229ede0262ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f28931ffb7bde8359bc12ad8aebe9e98e29c9a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("FileShareExpert")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FileShareExpert")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
